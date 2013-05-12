1d15d529c2c39c7c7823e97f9a2554eda1dd958b
Chris Dicken <cd4770@gmail.com>
2013-05-12 17:20:26 -0400

    Final Commit
    Includes Tests

---
+++ UnitTestsForTransEfficiencySuite/TransEfficiencySuiteUnitTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TransEfficiencySuite.Helpers;
+
+namespace UnitTestsForTransEfficiencySuite
+{
+    [TestClass]
+    public class TransEfficiencySuiteUnitTests
+    {
+        [TestMethod]
+        public void TestPostLength()
+        {
+            Assert.AreEqual(SignComputation.TotalPostLength("0", "24", "false"), "12.5");
+            Assert.AreEqual(SignComputation.TotalPostLength("1", "36", "false"), "13.5");
+            Assert.AreEqual(SignComputation.TotalPostLength("2", "12", "false"), "10.5");
+        }
+        [TestMethod]
+        public void TestPostType()
+        {
+            Assert.AreEqual(SignComputation.SignPostType("4", "24", "0", "true"), "Type 2");
+            Assert.AreEqual(SignComputation.SignPostType("9", "36", "0", "false"), "Type 2");
+            Assert.AreEqual(SignComputation.SignPostType("16", "48", "1", "false"), "Type 4");
+            Assert.AreEqual(SignComputation.SignPostType("25", "60", "1", "true"), "Type 6");
+        }
+
+
+    }
+}
